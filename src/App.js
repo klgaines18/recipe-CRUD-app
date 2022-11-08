6bac1d9d1cd0b322e3c8e43ad4d5a5c6e84764b9
Katie Gaines <klgaines18@gmail.com>
2022-11-08 06:49:17 -0500

    tie in app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
+  const createRecipe = (newRecipe) =>
+    setRecipes((currentRecipes) => [
+      ...currentRecipes,
+      newRecipe,
+  ]);
+  const deleteRecipe = (indexToDelete) =>
+    setRecipes((currentRecipes) =>
+      currentRecipes.filter((post, index) => index !== indexToDelete)
+  );
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
@@ -14,8 +23,11 @@ function App() {
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <RecipeList 
+        recipes={recipes}
+        deleteRecipe={deleteRecipe}
+      />
+      <RecipeCreate createRecipe={createRecipe} />
     </div>
   );
 }
